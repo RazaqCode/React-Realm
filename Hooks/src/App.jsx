84610cc532392a2ec971c01d3c995599c7bb6bc4
RazaqCode <razaqrealm@gmail.com>
2025-05-03 18:11:59 +0530

    imported useState

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react'
+import { useState } from 'react'  
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-
-  let counter  = 15;
+let [counter,setCounter] = useState(15)
+  //let counter  = 15;
 
 const addValue = () =>{
   console.log("clicked",counter);
-  counter  = counter + 1 ;
+  //counter  = counter + 1 ;
+  setCounter(counter + 1)
 
+}
+const removeValue = ()=>{
+setCounter(counter -1)
 }
 
   return (
     <>
      <h1>Rizz</h1>
      <h2>Counter value {counter}</h2>
-     <button onClick={addValue}>Add Value </button>
+     <button onClick={addValue}>Add Value {counter} </button>
      <br/>
      <br/>
-     <button>Remove Value </button>
+     <button onClick={removeValue}>Remove Value {counter}</button>
+     <p>footer: {counter}</p>
 
     </>
   )
