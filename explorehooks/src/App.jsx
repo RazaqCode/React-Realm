c5bc0327768426b4b9b87f48a700dc498dba52ac
RazaqCode <razaqrealm@gmail.com>
2025-05-12 21:37:24 +0530

    use state

--- explorehooks/src/App.jsx
+++ explorehooks/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed]= useState
+  (false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState ("")
   return (
     <>
      <h1 className = "text-4xl text-center text-white" >password generator</h1>
